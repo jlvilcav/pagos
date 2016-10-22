ac329005d1cbb5f52488e553fcf836837f23da71
Jorge Vilca <jlvilcav@gmail.com>
2016-10-21 21:44:09 -0500

    primer commit

---
+++ Pago.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Confirmación de Pago</title>
+	<link rel="stylesheet" type="text/css" href="icons/foundation-icons.css">
+    <link rel="stylesheet" href="css/foundation.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+	<p>El pago ha sido procesado, ingresando a Consultado de Grados <a href="http://www.sunedu.gob.pe/registro-nacional-de-grados-y-titulos/">aquí</a></p>
+</body>
+</html>
